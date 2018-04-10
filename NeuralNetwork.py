a717344a420f06689f1b594a00ddb2263d677cb4
Aluminiminium <aluminiminium@gmail.com>
2018-04-10 18:44:28 +0900

    commit test

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,3 +5,11 @@ Created on Tue Apr 10 17:33:53 2018
 @author: Tanaka-Lab-Desktop4
 """
 
+import numpy as np
+
+x = np.array([-1.0, 1.0, 2.0])
+print(x)
+
+y = x > 0
+y = y.astype(np.int)
+print( y )
